8ce07c091e6e5bc2e26e5efbad3ba88783fbe06a
Darsh <darshankondhare2698@gmail.com>
2022-12-12 15:06:15 +0530

    [darsh] Add: Serialization or Deserialization

--- FileIOOperation/BinarySerialization.cs
+++ FileIOOperation/BinarySerialization.cs
@@ -12,14 +12,14 @@ namespace FileIOOperation
         public static void Serialization()
         {
             Demo sample = new Demo();
-            FileStream fileStream = new FileStream(@"C:\GetRepositry\file_IO\FileIOOperation\Binary1.txt", FileMode.Create);
+            FileStream fileStream = new FileStream(@"C:\GetRepositry\file_IO\FileIOOperation\Binary.txt", FileMode.Create);
             BinaryFormatter formatter = new BinaryFormatter();
             formatter.Serialize(fileStream, sample);
             Console.ReadKey();
         }
         public static void Deserialization()
         {
-            FileStream fileStream = new FileStream(@"C:\GetRepositry\file_IO\FileIOOperation\Binary.txt", FileMode.Open);
+            FileStream fileStream = new FileStream(@"C:\GetRepositry\file_IO\FileIOOperation\Binary1.txt", FileMode.Open);
             BinaryFormatter formatter = new BinaryFormatter();
             Demo deserializedSampledemo = (Demo)formatter.Deserialize(fileStream);
             Console.WriteLine($"ApplicationName {deserializedSampledemo.Application_Name} --- ApplicationID {deserializedSampledemo.Application_ID}");
